94aeca56a90b23098977f77025c0ec8c955bd9b1
Mitchell Armstrong <mra0211@gmail.com>
2022-08-14 16:51:52 -0400

    Added a generatePassword function that prompts the user to type in how many characters long they would like their password to be while blank and null answers will return the user back to the beginning. Also created prompts asking the user if they want to include lowerCase, upperCase, numbers, and special characters.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+function generatePassword() {
+  var promptPassword = window.prompt("How long do you want your password to be? Your password must be between 8 and 128 characters long.")
+  var passwordLength = parseInt(promptPassword);
 
+  if (passwordLength === "" || passwordLength === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return generatePassword();
+  }
+
+  if (passwordLength != parseInt(passwordLength)) {
+    window.alert("You need to provide a numeric value! Please try again.");
+    return generatePassword();
+  } else {
+    console.log("You picked a numeric value!");
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
