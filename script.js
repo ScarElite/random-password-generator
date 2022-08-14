2d55979d3bfb95dfdf234f2282837e5138a55201
Mitchell Armstrong <mra0211@gmail.com>
2022-08-14 17:12:04 -0400

    Added a const variable to include all of the lowercase and uppercase letters of the alphabet, as well as the numbers 0-9, and special characters. Also added if statements to check if the user wanted to include certain character types and added an if statement so that the user has to select at least one type.

--- script.js
+++ script.js
@@ -8,11 +8,51 @@ function generatePassword() {
     return generatePassword();
   }
 
-  if (passwordLength != parseInt(passwordLength)) {
+  if (promptPassword.valueOf() < 8 || promptPassword.valueOf() > 128) {
+    window.alert("The length of your password must be between 8 and 128 characters.");
+    return generatePassword();
+  } else if (promptPassword.valueOf() >= 8 && promptPassword.valueOf() <= 128) {
+    console.log("You have picked a valid password length.");
+  }
+
+  if (promptPassword != parseInt(promptPassword)) {
     window.alert("You need to provide a numeric value! Please try again.");
     return generatePassword();
   } else {
     console.log("You picked a numeric value!");
+  }
+
+  var confirmLowerCase = window.confirm("Would you like your password to include a lowercase?");
+  var confirmUpperCase = window.confirm("Would you like your password to include an uppercase?");
+  var confirmNumber = window.confirm("Would you like your password to include a number?");
+  var confirmSpecial = window.confirm("Would you like your password to include a special character?");
+
+  const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+  const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const special = [".", ",", ":", "?", "=", "-", "(", ")", "/", "%", "@", "!", "&", "$", "#", "*",];
+
+  var characterTypes = [];
+
+  if (confirmLowerCase) {
+    characterTypes.push(lowerCase);
+  }
+
+  if (confirmUpperCase) {
+    characterTypes.push(upperCase);
+  }
+
+  if (confirmNumber) {
+    characterTypes.push(numbers);
+  }
+
+  if (confirmSpecial) {
+    characterTypes.push(special);
+  }
+
+  if (characterTypes.length === 0) {
+    window.alert("Your password must include at least one type of character! Please try again.");
+    return generatePassword();
   }
 }
 
